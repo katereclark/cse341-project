38b76a0a477bf179ebf2a4e13e21e01f24a5fa9d
katereclark <cla16022@byui.edu>
2022-01-11 16:43:34 -0700

    Added activity two

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,10 +3,28 @@
 const express = require('express');
 const router = express.Router();
 
+const usersList = [];
+
+router.post('/addUser', (req, res, next) => {
+  usersList.push(req.body.add);
+  console.log('User added');
+  res.redirect('/ta02/');
+});
+
+router.post('/removeUser', (req, res, next) => {
+  const index = usersList.indexOf(req.body.remove);
+  if (index !== -1) {
+    usersList.splice(index, 1);
+  }
+  console.log('User removed');
+  res.redirect('/ta02/');
+});
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
     path: '/ta02', // For pug, EJS
+    users: usersList,
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
   });
