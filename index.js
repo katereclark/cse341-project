bb822dd9b290b3a6cadc0b977a241c4170786601
katereclark <cla16022@byui.edu>
2022-01-26 11:14:19 -0700

    Added mongoDB and mongoose

--- index.js
+++ index.js
@@ -11,42 +11,72 @@
  * IMPORTANT: Make sure to run "npm install" in your root before "npm start"
  *******************************************************************************/
 // Our initial setup (package requires, port number setup)
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
 
 const app = express();
 
+const cors = require("cors"); // Place this with other requires (like 'path' and 'express')
+
+const corsOptions = {
+  origin: "https://cse341-project.herokuapp.com/",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  family: 4,
+};
+
+const MONGODB_URL =
+  process.env.MONGODB_URL ||
+  "mongodb+srv://katerclark:e2e1gGS6Cc3DtZRx@cluster0.r8pqt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
 // Route setup. You can implement more in the future!
-const ta01Routes = require('./routes/ta01');
-const ta02Routes = require('./routes/ta02');
-const ta03Routes = require('./routes/ta03');
-const ta04Routes = require('./routes/ta04');
+const ta01Routes = require("./routes/ta01");
+const ta02Routes = require("./routes/ta02");
+const ta03Routes = require("./routes/ta03");
+const ta04Routes = require("./routes/ta04");
 
 app
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
   // For view engine as Pug
   //.set('view engine', 'pug') // For view engine as PUG.
   // For view engine as hbs (Handlebars)
   //.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'})) // For handlebars
   //.set('view engine', 'hbs')
   .use(bodyParser.urlencoded({ extended: false })) // For parsing the body of a POST
-  .use('/ta01', ta01Routes)
-  .use('/ta02', ta02Routes)
-  .use('/ta03', ta03Routes)
-  .use('/ta04', ta04Routes)
-  .get('/', (req, res, next) => {
+  .use("/ta01", ta01Routes)
+  .use("/ta02", ta02Routes)
+  .use("/ta03", ta03Routes)
+  .use("/ta04", ta04Routes)
+  .get("/", (req, res, next) => {
     // This is the primary index, always handled last.
-    res.render('pages/index', {
-      title: 'Welcome to my CSE341 repo',
-      path: '/',
+    res.render("pages/index", {
+      title: "Welcome to my CSE341 repo",
+      path: "/",
     });
   })
   .use((req, res, next) => {
     // 404 page
-    res.render('pages/404', { title: '404 - Page Not Found', path: req.url });
+    res.render("pages/404", { title: "404 - Page Not Found", path: req.url });
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+mongoose
+  .connect(MONGODB_URL, options)
+  .then((result) => {
+    // This should be your user handling code implement following the course videos
+    app.listen(PORT);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
